5053c0d1f802a3194397f5fd06dd160fec5c5cb5
Elias Haroun <eliha@amazon.com>
2025-10-03 11:35:32 -0700

    fix build

--- next.config.js
+++ next.config.js
@@ -5,10 +5,6 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_GA_ID: 'G-9TEKB5BMBM',
   },
-  // Enable analytics in production
-  experimental: {
-    instrumentationHook: true,
-  },
 }
 
 module.exports = nextConfig
