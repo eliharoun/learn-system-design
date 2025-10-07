e8ed6d0f903db46b3e70e8cf06aacdc341624f35
Elias Haroun <eliha@amazon.com>
2025-10-06 17:26:31 -0700

    Add favicon

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -109,10 +109,8 @@ export const SEO: React.FC<SEOProps> = ({
       <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
 
       {/* Favicon */}
-      <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" href="/favicon.png" />
+      <link rel="apple-touch-icon" href="/favicon.png" />
 
       {/* Structured Data */}
       {structuredData && (
